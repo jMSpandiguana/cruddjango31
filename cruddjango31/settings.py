9730f224f8bf351cd78f7a63cedd94e0493b0448
jMSpandiguana <pandiguana@gmail.com>
2022-10-18 16:44:22 -0500

    requerimientos maerial

--- cruddjango31/settings.py
+++ cruddjango31/settings.py
@@ -35,10 +35,10 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    #'material',
-    #'material.admin',
+    'material',
+    'material.admin',
 
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
